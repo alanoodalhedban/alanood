6eea6ab0d0e53a27a2c48011d964f31387672742
alanoodalhedban <>
2021-05-05 11:37:55 +0000

    add js file

---
+++ app.js
@@ -0,0 +1,24 @@
+alert('Welcome to our store')
+prompt("what do you want color flower ?");
+var colorflower = prompt("what do you want color flower ?")
+
+  if (colorflower ==='red')
+   {
+     alert('the price :10JD')
+   }
+   else if(colorflower === 'white')
+   {
+     alert('the price :15JD')
+   }
+   else if (colorflower === 'yellow')
+   {
+     alert('the price :20JD')
+    
+   }
+   else 
+   {
+     alert ('You must choose which flower you want to buy')
+   }
+
+   confirm('Do you want to buy now?')
+
